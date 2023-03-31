9f1888005a32d6020f469a922a41680422a68ebf
cesarlopezt <cesarlopez2808@gmail.com>
2023-03-30 23:19:21 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Cesar Lopez on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
